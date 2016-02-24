haplo-org/haplo-standard-plugins-dev

commit 193c765cc40ef3468794e33cc2082f15f7b364fc

Added deferredRender() to list dashboards.

std_reporting/js/std_reporting_dashboard_list.js
@@ -28,7 +28,7 @@ DashboardList.prototype.columns = function(priority, columns) {
     return this;
 };
 
-DashboardList.prototype.respond = function() {
+DashboardList.prototype._makeDashboardView = function() {
     // Make sorted flat list of column objects
     var columns = [];
     var sortedGroups = _.each(_.sortBy(this.columnGroups, "priority"), function(group) {
@@ -55,8 +55,7 @@ DashboardList.prototype.respond = function() {
         });
     });
 
-    this.E.setResponsiblePlugin(P);
-    this.E.render({
+    return {
         pageTitle: this.specification.title,
         backLink: this.specification.backLink,
         backLinkText: this.specification.backLinkText,
@@ -65,8 +64,17 @@ DashboardList.prototype.respond = function() {
         widgetsTop: _.map(this.$uiWidgetsTop, function(f) { return f(); }),
         columns: columns,
         rowsHTML: rowsHTML
-    }, "dashboard/list/list_dashboard");
+    };
+};
+
+DashboardList.prototype.respond = function() {
+    this.E.setResponsiblePlugin(P);
+    this.E.render(this._makeDashboardView(), "dashboard/list/list_dashboard");
     return this;
+};
+
+DashboardList.prototype.deferredRender = function() {
+    return P.template("dashboard/list/list_dashboard").deferredRender(this._makeDashboardView());
 };
 
 DashboardList.prototype._respondWithExport = function(columns) {

commit c6ee43351085e7dd22ba3583c8fb30a0c80cdec1

Fixed dashboard export bug introduced in 193c765cc40ef3468794e33cc2082f15f7b364fc.

std_reporting/js/std_reporting_dashboard_list.js
@@ -28,15 +28,17 @@ DashboardList.prototype.columns = function(priority, columns) {
     return this;
 };
 
-DashboardList.prototype._makeDashboardView = function(hideExport) {
+DashboardList.prototype._makeSortedColumnList = function() {
     // Make sorted flat list of column objects
     var columns = [];
-    var sortedGroups = _.each(_.sortBy(this.columnGroups, "priority"), function(group) {
+    _.each(_.sortBy(this.columnGroups, "priority"), function(group) {
         columns = columns.concat(group.columns);
     });
+    return columns;
+};
 
-    // Export data rather than rendering?
-    if(this.isExporting) { return this._respondWithExport(columns); }
+DashboardList.prototype._makeDashboardView = function(hideExport) {
+    var columns = this._makeSortedColumnList();
 
     // Locally scoped copy of row attribute generator functions
     var rowAttributeFns = this.$rowAttributeFns;
@@ -70,6 +72,8 @@ DashboardList.prototype._makeDashboardView = function(hideExport) {
 
 DashboardList.prototype.respond = function() {
     this.E.setResponsiblePlugin(P);
+    // Export data rather than rendering?
+    if(this.isExporting) { return this._respondWithExport(); }
     this.E.render(this._makeDashboardView(), "dashboard/list/list_dashboard");
     return this;
 };
@@ -78,7 +82,8 @@ DashboardList.prototype.deferredRender = function() {
     return P.template("dashboard/list/list_dashboard").deferredRender(this._makeDashboardView(true /* Hide Export */));
 };
 
-DashboardList.prototype._respondWithExport = function(columns) {
+DashboardList.prototype._respondWithExport = function() {
+    var columns = this._makeSortedColumnList();
     var xls = O.generate.table.xlsx(this.specification.title);
     xls.newSheet(this.specification.title, true);
     _.each(columns, function(c) {