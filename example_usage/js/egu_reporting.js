a8e60e79b1e353e5b9f69d4f650fee436a34404d
Ben Summers <ben.summers@haplo-services.com>
2016-08-28 14:10:45 +0100

    Example: Add simple reporting collection and dashboard

--- example_usage/js/egu_reporting.js
+++ example_usage/js/egu_reporting.js
@@ -1,9 +1,8 @@
 
 P.implementService("std:action_panel:example_reporting", function(display, builder) {
-    builder.panel(100).link(100,
-        "/do/example-usage/states-dashboard/",
-        "Workflow states",
-        "default");
+    builder.panel(100).
+        link(100, "/do/example-usage/states-dashboard/", "Workflow states", "standard").
+        link(101, "/do/example-usage/reporting-dashboard", "Reporting dashboard", "standard");
 });
 
 // Workflow dashboard documentation: http://docs.haplo.org/dev/standard-plugin/workflow/definition/std-features/states-dashboard
@@ -17,4 +16,35 @@ P.ExampleUsageWorkflow.use("std:dashboard:states", {
         "wait_submit",
         "finished"
     ]
+});
+
+// --------------------------------------------------------------------------
+
+P.implementService("std:reporting:discover_collections", function(discover) {
+    discover("example", "Example");
+});
+
+P.implementService("std:reporting:collection:example:setup", function(collection) {
+    collection.
+        currentObjectsOfType(T.File).
+        fact("numberOfAuthors", "int", "Number of authors");
+});
+
+P.implementService("std:reporting:collection:example:get_facts_for_object", function(object, row) {
+    row.numberOfAuthors = object.every(A.Author).length;
+});
+
+P.respond("GET,POST", "/do/example-usage/reporting-dashboard", [
+], function(E) {
+    P.reporting.dashboard(E, {
+        name: "example_dashboard",
+        kind: "list",
+        collection: "example",
+        title: "Example dashboard"
+    }).
+        columns(100, [
+            {fact:"ref", heading:"File"},
+            "numberOfAuthors"
+        ]).
+        respond();
 });
