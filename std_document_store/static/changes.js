ad4a7a97c824c79de728114df60ec8dc1055b865
Helen Root <helen.root@haplo-services.com>
2018-03-21 17:46:28 +0000

    Use boolean returned by oForms.display to determine whether to display a no changes warning

--- std_document_store/static/changes.js
+++ std_document_store/static/changes.js
@@ -7,12 +7,17 @@
         var configDiv = $('#z__docstore_changes_configuration')[0],
             filterOn = configDiv.getAttribute('data-filter') === "1";
 
+        var hasChanges = false;
         $('#z__docstore_body .z__docstore_form_display').each(function() {
             var previousElement = document.getElementById("_prev_"+this.id);
             if(previousElement) {
-                oFormsChanges.display($('.oform',this)[0], $('.oform',previousElement)[0], false);
+                hasChanges = oFormsChanges.display($('.oform',this)[0], $('.oform',previousElement)[0], false) || hasChanges;
             }
         });
+
+        if(!hasChanges) {
+            $('#z__no_changes_warning').show();
+        }
 
         $('#z__docstore_body .z__docstore_form_display').each(function() {
             oFormsChanges.unchangedVisibility(this, !filterOn);
