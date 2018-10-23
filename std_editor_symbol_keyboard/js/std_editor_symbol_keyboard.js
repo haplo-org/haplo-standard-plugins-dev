bd7b78986273ee082bac0f6849b3d7be04018034
Ben Summers <ben.summers@haplo-services.com>
2018-10-23 15:55:09 +0100

    Apply license header.

--- std_editor_symbol_keyboard/js/std_editor_symbol_keyboard.js
+++ std_editor_symbol_keyboard/js/std_editor_symbol_keyboard.js
@@ -1,3 +1,9 @@
+/* Haplo Platform                                    https://haplo.org
+ * (c) Haplo Services Ltd 2006 - 2018            https://www.haplo.com
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.         */
+
 
 const DEFAULT_KEYBOARDS = {"Greek":"ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδϵζηθικλμνξοπρστυϕχψω","Maths":"=≠≈><≥≤+−±×⋅÷/√"};
 
