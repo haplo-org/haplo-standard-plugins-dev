7656876b7e2e705d6d417fd0df70d81a51cc11f3
Ben Summers <ben.summers@haplo.com>
2020-01-08 10:20:40 +0000

    Fix source ordering

--- std_workflow/js/std_workflow_serialisation.js
+++ std_workflow/js/std_workflow_serialisation.js
@@ -3,7 +3,7 @@ P.implementService("std:serialiser:discover-sources", function(source) {
     source({
         name: "std:workflow",
         depend: "std:workunit",
-        sort: 1000,
+        sort: 1100,
         setup(serialiser) {
             serialiser.listen("std:workunit:extend", function(workUnit, work) {
                 let wdefn = O.service("std:workflow:definition_for_name", workUnit.workType);
