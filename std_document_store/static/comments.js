2c70deae377a85cd110d5b7b1d073f27abc4fde3
Helen Root <helen.root@haplo-services.com>
2020-02-26 12:29:33 +0000

    One line will do

--- std_document_store/static/comments.js
+++ std_document_store/static/comments.js
@@ -144,14 +144,7 @@
         if(canAddComment) {
             $('#z__docstore_body div[data-uname]').each(function() {
                 // Ignore if this contains other elements with unames, unless all of those elements are in one table
-                if($('[data-uname]',this).length) {
-                    var allPartOfTable = true;
-                    $('[data-uname]',this).each(function(idx) {
-                        if(!allPartOfTable) { return; }
-                        allPartOfTable = $(this).is('td');
-                    });
-                    if(!allPartOfTable) { return; }
-                }
+                if($('[data-uname]:not(td)',this).length) { return; }
                 if(!/\S/.test(this.innerText||'')) { return; }  // no text/labels to comment on
                 $(this).prepend('<div class="z__docstore_add_comment"><a class="z__docstore_add_comment_button" href="#" title="Add comment">Add comment<span></span></a></div>');
             });
