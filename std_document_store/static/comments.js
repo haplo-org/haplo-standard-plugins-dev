6420619bf4a9fab6f847f366f0a8aae134fb3e92
James Roberts <jde.roberts3@gmail.com>
2024-05-13 17:57:30 +0100

    Docstore comments: generate the add comment form server-side to ensure an up to date CSRF token

--- std_document_store/static/comments.js
+++ std_document_store/static/comments.js
@@ -13,6 +13,7 @@
             onlyViewingCommentsForForm = configDiv.getAttribute('data-onlyform'),
             canAddComment = !!configDiv.getAttribute('data-add'),
             commentServerUrl = configDiv.getAttribute('data-url'),
+            commentFormUrl = configDiv.getAttribute('data-comments-form-url'),
             isViewer = !!configDiv.getAttribute('data-isviewer'),
             filterOn = configDiv.getAttribute('data-filter') === "1",
             showingChanges = configDiv.getAttribute('data-changes') === "1",
@@ -158,47 +159,37 @@
             });
 
             var showAddComment = function(that, commentId, text, isPrivate) {
-                var commentBoxHtml = '<div class="z__docstore_comment_enter_ui';
-                if(privateCommentsEnabled && (isPrivate !== false)) {
-                    // if isPrivate is true or undefined, so that we get private comments by default, set private class
-                    commentBoxHtml += ' z__docstore_private_comment"';
-                } else {
-                    commentBoxHtml += '"';
-                }
-                if(commentId) { commentBoxHtml += 'data-commentid="'+commentId+'"'; }
-                commentBoxHtml += '><span><textarea rows="4">'+(text ? _.escape(text) : '')+'</textarea></span>';
-                if(privateCommentsEnabled) {
-                    commentBoxHtml += '<label>';
-                    if(!addPrivateCommentOnly) {
-                        commentBoxHtml += '<input type="checkbox" id="commment_is_private" name="private" value="yes"';
-                        commentBoxHtml += (isPrivate !== false) ? 'checked="checked">' : '>';
+                // Request a fresh comment form from the server, inc. valid form token
+                $.ajax(commentFormUrl, {
+                    data: {
+                        id: commentId,
+                        private: isPrivate
+                    },
+                    success: function(html, textStatus, jqXHR) {
+                        if(textStatus !== "success") {
+                            window.alert("Failed to load comment form");
+                            return;
+                        }
+                        var commentBox = $(html);
+                        var element = $(that).parents('[data-uname]').first();
+                        var existingComments = $('.z__docstore_comment_container', element);
+                        if(existingComments.length) {
+                            existingComments.first().before(commentBox);
+                        } else {
+                            element.append(commentBox);
+                        }
+                        return commentBox;
                     }
-                    commentBoxHtml += _.escape(addPrivateCommentLabel);
-                    commentBoxHtml += '</label>';
-                }
-                commentBoxHtml += '<div class="z__docstore_comment_enter_controls">'+
-                    '<a href="#" class="z__docstore_comment_enter_cancel">cancel</a> '+
-                    '<input class="z__docstore_comment_enter_submit" type="submit" value="Save comment">'+
-                    '</div></div>';
-                var commentBox = $(commentBoxHtml);
-
-                var element = $(that).parents('[data-uname]').first();
-                var existingComments = $('.z__docstore_comment_container', element);
-                if(existingComments.length) {
-                    existingComments.first().before(commentBox);
-                } else {
-                    element.append(commentBox);
-                }
-                return commentBox;
+                });
             };
 
             $('#z__docstore_body').on('click', '.z__docstore_add_comment_button', function(evt) {
                 evt.preventDefault();
-                var commentBox = showAddComment(this, undefined, undefined, defaultCommentIsPrivate);
+                showAddComment(this, undefined, undefined, defaultCommentIsPrivate);
                 $(this).hide(); // hide button to avoid
                 var element = $(this).parents('[data-uname]').first();
                 $(element).find(".z__docstore_comment_footer").hide();
-                window.setTimeout(function() { $('textarea',commentBox).focus(); }, 1);
+                window.setTimeout(function() { $('textarea',$(element)).focus(); }, 100);
             });
 
             $('#z__docstore_body').on('click', '.z__docstore_edit_comment_link', function(evt) {
@@ -209,12 +200,12 @@
                 }).join("\n");
                 var isPrivate = commentContainer.hasClass('z__docstore_private_comment');
                 var commentToSupersede = commentContainer[0].getAttribute('data-commentid');
-                var commentBox = showAddComment(this, commentToSupersede, text, isPrivate);
+                showAddComment(this, commentToSupersede, text, isPrivate);
                 commentContainer.hide(); // hide comment
                 var element = $(this).parents('[data-uname]').first();
                 $(element).find(".z__docstore_add_comment_button").hide();
                 $(element).find(".z__docstore_comment_footer").hide();
-                window.setTimeout(function() { $('textarea',commentBox).focus(); }, 1);
+                window.setTimeout(function() { $('textarea',$(element)).focus(); }, 100);
             });
 
             var restoreCommentControls = function(that, toSupersede) {
@@ -267,7 +258,7 @@
                 if(comment || commentToSupersede) {
                     var formId = element.parents('.z__docstore_form_display').first()[0].id,
                         uname = element[0].getAttribute('data-uname'),
-                        token = $('#z__docstore_comments_configuration input[name=__]')[0].value;   // CSRF token
+                        token = $('input[name=__]', element)[0].value;   // CSRF token
                     $.ajax(commentServerUrl, {
                         method: "POST",
                         data: {
@@ -291,7 +282,7 @@
                             displayComment(formId, uname, data.comment, true /* at top, so reverse ordered by date to match viewing */);
                         },
                         error: function(data) {
-                            window.alert("Failed to add comment, please try again.\nIf the problem persists, try refreshing the page before re-adding your comment.");
+                            window.alert("Failed to add comment, please try again.\nIf the problem persists, try re-adding your comment after refreshing the page.");
                             toggleCommentControls(that, true);
                         }
                     });
